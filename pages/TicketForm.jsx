b89902db53bf2b8fe04610399f8d781f799640e0
joseph grace <gracecjoseph5@gmail.com>
2024-10-16 12:37:25 +0100

    Still working on the ticket page

--- pages/TicketForm.jsx
+++ pages/TicketForm.jsx
@@ -1,32 +1,61 @@
-import Layout from "@/components/layout/Layout";
 import { useRouter } from "next/router";
-import moviesData from "@/components/movieSchedule/moviesData";
 
-const TicketForm = () => {
+const TicketForm = ({ onProceed }) => {
   const router = useRouter();
 
   const handleBack = () => {
-    router.back()
+    router.back();
   };
   return (
-    <Layout>
-      <div className="h-screen w-full bg-[url('/img/ticket2.jpg')] bg-no-repeat bg-cover bg-top"></div>
-      <form>
-        <button onClick={handleBack}>back</button>
-        <div>
-          <label htmlFor="">Name</label>
-          <input type="text" name="" id="" />
-        </div>
-        <div>
-          <label htmlFor="">Email</label>
-          <input type="email" name="" id="" />
-        </div>
-        <div>
-          <label htmlFor="">Phone number</label>
-          <input type="number" name="" id="" />
-        </div>
-      </form>
-    </Layout>
+    <>
+      <div className="px-[5%] py-8">
+        <button
+          onClick={handleBack}
+          className="border border-purple px-4 text-[1rem] rounded-md"
+        >
+          &#8592;
+        </button>
+        <form className=" flex flex-col justify-center items-center gap-5">
+          <div className="flex flex-col w-[40%]">
+            <label htmlFor="" className="text-purple">
+              Name
+            </label>
+            <input
+              type="text"
+              name=""
+              id=""
+              placeholder="What's your name?"
+              className="text-black text-sm px-4 py-1 outline-none rounded-md"
+            />
+          </div>
+          <div className="flex flex-col w-[40%]">
+            <label htmlFor="" className="text-purple">
+              Email
+            </label>
+            <input
+              type="email"
+              name=""
+              id=""
+              placeholder="Enter your email..."
+              className="text-black text-sm px-4 py-1 outline-none rounded-md"
+            />
+          </div>
+          <div className="flex flex-col w-[40%]">
+            <label htmlFor="" className="text-purple">
+              Phone number
+            </label>
+            <input
+              type="number"
+              name=""
+              id=""
+              placeholder="Phone number..."
+              className="text-black text-sm px-4 py-1 outline-none rounded-md"
+            />
+          </div>
+          <button onClick={onProceed}>Proceed</button>
+        </form>
+      </div>
+    </>
   );
 };
 
--- pages/movies/[id].js
+++ pages/movies/[id].js
@@ -7,13 +7,12 @@ import TicketForm from "@/pages/TicketForm";
 
 
 const TicketDetails = () => {
+  const [screenState, setScreenState] = useState(0);
   const [quantity, setQuantity] = useState(0);
   const [error, setError] = useState("");
   const router = useRouter();
   const { id } = router.query;
 
-  //  const increment = () => setQuantity(quantity + 1);
-  //  const decrement = () => setQuantity(quantity > 0 ? quantity - 1 : 0);
   const increment = () => {
     setError("")
     setQuantity(quantity + 1);
@@ -26,7 +25,7 @@ const TicketDetails = () => {
 
   const handleProceed = () => {
     if (quantity > 0) {
-      router.push("/TicketForm");
+      setScreenState(screenState + 1); 
     } else {
       setError("Please select the number of tickets.");
     }
@@ -42,57 +41,46 @@ const TicketDetails = () => {
   return (
     <Layout>
       <div className="h-screen w-full bg-[url('/img/ticket2.jpg')] bg-no-repeat bg-cover bg-top"></div>
-      <div className="px-[5%] my-[50px]">
-        <div className="flex items-center gap-14 bg-gray-900 rounded-lg">
-          <Image src={movie.img} alt={movie.title} className="rounded-lg" />
-          <div>
-            <h2 className="text-[2rem]">
-              <span>Movie Title : </span>
-              {movie.title}
-            </h2>
-            <p className="text-[1rem]">
-              <span className="text-purple">Genre: </span>
-              {movie.genre}
-            </p>
-            <p className="text-[1rem]">
-              <span className="text-purple">Movie Duration: </span>duration
-            </p>
+      {screenState === 0 && (
+        <div className="px-[5%] my-[50px]">
+          <div className="flex items-center gap-14 bg-gray-900 rounded-lg">
+            <Image src={movie.img} alt={movie.title} className="rounded-lg" />
+            <div>
+              <h2 className="text-[2rem]">Movie Title : {movie.title}</h2>
+              <p className="text-[1rem]">Genre: {movie.genre}</p>
+              <p className="text-[1rem]">Duration: {movie.duration}</p>
+            </div>
+          </div>
+          <div className="my-[50px]">
+            <p className="my-6 text-[2rem] text-purple">Number of Tickets</p>
+            <div className="flex gap-6">
+              <button
+                onClick={decrement}
+                className="text-black font-bold  px-4 border-purple bg-white rounded-full shadow-[1px_2px_0_1px_#ba10ba]"
+              >
+                -
+              </button>
+              <span className="text-white font-bold border px-4 border-purple rounded-full shadow-[1px_2px_0_1px_#ba10ba]">
+                {quantity}
+              </span>
+              <button
+                onClick={increment}
+                className="text-black font-bold  px-4 border-purple bg-white rounded-full shadow-[1px_2px_0_1px_#ba10ba]"
+              >
+                +
+              </button>
+            </div>
+            <button
+              onClick={handleProceed}
+              className="mt-6 bg-purple text-white px-6 py-1 rounded-md"
+            >
+              Proceed
+            </button>
+            {error && <p className="text-red-500 mt-2">{error}</p>}
           </div>
         </div>
-        <div className="my-[50px]">
-          <p className="my-6 text-[2rem] text-purple">Number of Tickets</p>
-          <div className="flex gap-6">
-            <button
-              onClick={decrement}
-              className="text-black font-bold  px-4 border-purple bg-white rounded-full shadow-[1px_2px_0_1px_#ba10ba]"
-            >
-              -
-            </button>
-            <span className="text-white font-bold border px-4 border-purple rounded-full shadow-[1px_2px_0_1px_#ba10ba]">
-              {quantity}
-            </span>
-            <button
-              onClick={increment}
-              className="text-black font-bold  px-4 border-purple bg-white rounded-full shadow-[1px_2px_0_1px_#ba10ba]"
-            >
-              +
-            </button>
-          </div>
-          <button
-            onClick={handleProceed}
-            disabled={quantity === 0}
-            className={`mt-6 border px-6 py-1 rounded-md cursor-pointer ${
-              quantity === 0
-                ? "bg-gray-900 text-white"
-                : "bg-purple text-white"
-            }`}
-          >
-            Proceed
-          </button>
-          {error && <p className="text-red-500 mt-2">{error}</p>}
-          {/* <TicketForm /> */}
-        </div>
-      </div>
+      )}
+      {screenState === 1 && <TicketForm onProceed={handleProceed} />}
     </Layout>
   );
 };
